6c032eeb9ce5ad43a1f007d46eaf874edfb3db45
MohamedAttia3104 <attia3104@gmail.com>
2024-08-29 13:24:49 +0300

    build sign up repository

--- lib/core/networking/api_services.dart
+++ lib/core/networking/api_services.dart
@@ -1,8 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_advanced_complete_course/features/login/data/models/login_request_body.dart';
 import 'package:flutter_advanced_complete_course/features/login/data/models/login_response.dart';
+import 'package:flutter_advanced_complete_course/features/signup/data/models/sign_up_response.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:flutter_advanced_complete_course/core/networking/api_constants.dart';
+
+import '../../features/signup/data/models/sign_up_request_body.dart';
 
 part 'api_services.g.dart';
 
@@ -15,5 +18,8 @@ abstract class ApiServices {
     @Body() LoginRequestBody loginRequestBody,
   );
 
-  
+  @POST(ApiConstants.signUp)
+  Future<SignUpResponse> signUp(
+    @Body() SignUpRequestBody signUpRequestBody,
+  );
 }
